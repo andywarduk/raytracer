da7792a5400719cdbfccb326d93fe6132a245171
Andy Ward <andy.ward.uk@gmail.com>
2024-06-24 23:25:05 +0100

    More key controls

--- binlib/src/lib.rs
+++ binlib/src/lib.rs
@@ -9,7 +9,7 @@ use std::{
 use clap::Parser;
 use raytracer_lib::{
     camera::{CamProgressCb, Camera},
-    triple::Colour,
+    triple::{Colour, Vec3},
 };
 
 pub trait Renderer {
@@ -196,27 +196,304 @@ fn render_to_window(mut cam: Camera, state: impl Renderer) -> Result<(), Box<dyn
 fn process_keys(cam: &mut Camera, keys: &mut Vec<u32>) -> bool {
     let mut clear = false;
 
+    const SMALL_MOVE: f64 = 10.0;
+    const BIG_MOVE: f64 = 100.0;
+
     for t in keys.iter() {
         match char::from_u32(*t) {
             // Vertical FOV
-            Some('f') => adjust_vfov(cam, 1.0, &mut clear),
-            Some('F') => adjust_vfov(cam, 5.0, &mut clear),
-            Some('v') => adjust_vfov(cam, -1.0, &mut clear),
-            Some('V') => adjust_vfov(cam, -5.0, &mut clear),
+            Some('z') => adjust_vfov(cam, -1.0, &mut clear),
+            Some('Z') => adjust_vfov(cam, -5.0, &mut clear),
+            Some('x') => adjust_vfov(cam, 1.0, &mut clear),
+            Some('X') => adjust_vfov(cam, 5.0, &mut clear),
             // Focus
-            Some('g') => adjust_focus(cam, 0.1, 0.0, &mut clear),
-            Some('G') => adjust_focus(cam, 1.0, 0.0, &mut clear),
-            Some('b') => adjust_focus(cam, -0.1, 0.0, &mut clear),
-            Some('B') => adjust_focus(cam, -1.0, 0.0, &mut clear),
-            Some('h') => adjust_focus(cam, 0.0, 0.1, &mut clear),
-            Some('H') => adjust_focus(cam, 0.0, 1.0, &mut clear),
-            Some('n') => adjust_focus(cam, 0.0, -0.1, &mut clear),
-            Some('N') => adjust_focus(cam, 0.0, -1.0, &mut clear),
+            Some('c') => adjust_focus(cam, -0.1, 0.0, &mut clear),
+            Some('C') => adjust_focus(cam, -1.0, 0.0, &mut clear),
+            Some('v') => adjust_focus(cam, 0.1, 0.0, &mut clear),
+            Some('V') => adjust_focus(cam, 1.0, 0.0, &mut clear),
+            Some('b') => adjust_focus(cam, 0.0, -0.1, &mut clear),
+            Some('B') => adjust_focus(cam, 0.0, -1.0, &mut clear),
+            Some('n') => adjust_focus(cam, 0.0, 0.1, &mut clear),
+            Some('N') => adjust_focus(cam, 0.0, 1.0, &mut clear),
             // Depth
-            Some('z') => adjust_depth(cam, -1, &mut clear),
-            Some('Z') => adjust_depth(cam, -5, &mut clear),
-            Some('x') => adjust_depth(cam, 1, &mut clear),
-            Some('X') => adjust_depth(cam, 5, &mut clear),
+            Some('[') => adjust_depth(cam, -1, &mut clear),
+            Some('{') => adjust_depth(cam, -5, &mut clear),
+            Some(']') => adjust_depth(cam, 1, &mut clear),
+            Some('}') => adjust_depth(cam, 5, &mut clear),
+            // Camera position
+            Some('q') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, -SMALL_MOVE),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('Q') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, -BIG_MOVE),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('w') => adjust_view(
+                cam,
+                Vec3::new(0.0, SMALL_MOVE, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('W') => adjust_view(
+                cam,
+                Vec3::new(0.0, BIG_MOVE, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('e') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, SMALL_MOVE),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('E') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, BIG_MOVE),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('a') => adjust_view(
+                cam,
+                Vec3::new(-SMALL_MOVE, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('A') => adjust_view(
+                cam,
+                Vec3::new(-BIG_MOVE, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('s') => adjust_view(
+                cam,
+                Vec3::new(0.0, -SMALL_MOVE, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('S') => adjust_view(
+                cam,
+                Vec3::new(0.0, -BIG_MOVE, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('d') => adjust_view(
+                cam,
+                Vec3::new(SMALL_MOVE, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('D') => adjust_view(
+                cam,
+                Vec3::new(BIG_MOVE, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            // Camera target
+            Some('r') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, -SMALL_MOVE),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('R') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, -BIG_MOVE),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('t') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, SMALL_MOVE, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('T') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, BIG_MOVE, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('y') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, SMALL_MOVE),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('Y') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, BIG_MOVE),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('f') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(-SMALL_MOVE, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('F') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(-BIG_MOVE, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('g') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, -SMALL_MOVE, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('G') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, -BIG_MOVE, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('h') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(SMALL_MOVE, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('H') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(BIG_MOVE, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                &mut clear,
+            ),
+            // Camera orienation
+            Some('u') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, -SMALL_MOVE),
+                &mut clear,
+            ),
+            Some('U') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, -BIG_MOVE),
+                &mut clear,
+            ),
+            Some('i') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, SMALL_MOVE, 0.0),
+                &mut clear,
+            ),
+            Some('I') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, BIG_MOVE, 0.0),
+                &mut clear,
+            ),
+            Some('o') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, SMALL_MOVE),
+                &mut clear,
+            ),
+            Some('O') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, BIG_MOVE),
+                &mut clear,
+            ),
+            Some('j') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(-SMALL_MOVE, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('J') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(-BIG_MOVE, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('k') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, -SMALL_MOVE, 0.0),
+                &mut clear,
+            ),
+            Some('K') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, -BIG_MOVE, 0.0),
+                &mut clear,
+            ),
+            Some('l') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(SMALL_MOVE, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('L') => adjust_view(
+                cam,
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(BIG_MOVE, 0.0, 0.0),
+                &mut clear,
+            ),
+            Some('?') => {
+                println!("Key bindings:");
+                println!(" Camera position:");
+                println!("   Q/q W/w E/e => Z- Y+ Z+");
+                println!("   A/a S/s D/d => X- Y- X+ ");
+                println!(" Camera target:");
+                println!("   R/r T/t Y/y => Z- Y+ Z+");
+                println!("   F/f G/g H/h => X- Y- X+ ");
+                println!(" Camera orientation:");
+                println!("   U/u I/i O/o => Z- Y+ Z+");
+                println!("   J/j K/k L/l => X- Y- X+ ");
+                println!(" Vertical FOV:");
+                println!("   z/Z x/X => Decrease / increase vertical field of vision angle");
+                println!(" Focus:");
+                println!("   c/C x/X => Decrease / increase defocus angle");
+                println!("   v/V b/B => Decrease / increase focus distance");
+                println!(" Depth:");
+                println!("   [/{{ ]/}} => Decrease / increase ray depth (number of bounces)");
+            }
             // Catch others
             _ => (),
         }
@@ -260,6 +537,23 @@ fn adjust_depth(cam: &mut Camera, adjustment: i64, clear: &mut bool) {
     }
 }
 
+fn adjust_view(
+    cam: &mut Camera,
+    look_from_adj: Vec3,
+    look_at_adj: Vec3,
+    vup_adj: Vec3,
+    clear: &mut bool,
+) {
+    let (look_from, look_at, vup) = cam.view();
+
+    let new_look_from = look_from + look_from_adj;
+    let new_look_at = look_at + look_at_adj;
+    let new_vup = vup + vup_adj;
+
+    cam.set_view(new_look_from, new_look_at, new_vup);
+    *clear = true;
+}
+
 fn dump_camera_parameters(cam: &Camera, show_samples: bool) {
     println!("Camera parameters:");
 
@@ -276,7 +570,10 @@ fn dump_camera_parameters(cam: &Camera, show_samples: bool) {
     println!("          Image dimensions : {w} x {h}");
     println!("                 Look from : {look_from}");
     println!("                   Look to : {look_at}");
-    println!("            -> Look vector : {view_vec}, distance {}", view_vec.length());
+    println!(
+        "            -> Look vector : {view_vec}, distance {}",
+        view_vec.length()
+    );
     println!("                        Up : {vup}");
     println!("  Vertical field of vision : {vfov}°");
     println!("             Defocus angle : {defocus_angle}°");
